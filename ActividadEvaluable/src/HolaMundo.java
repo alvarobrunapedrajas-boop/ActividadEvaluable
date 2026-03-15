7bab20a1f0eb9a863e755ef83de8c24c1fffb9b9
User <User@DESKTOP-CABPFFI>
2026-03-15 12:18:22 +0100

    Validación del programa: cambio dos

--- ActividadEvaluable/src/HolaMundo.java
+++ ActividadEvaluable/src/HolaMundo.java
@@ -3,7 +3,9 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Hola Mundo");
+		System.out.println("Actividad Evaluable: Git-Eclipse");
+		System.out.println("Cambio 1");
 	}
 
 }
